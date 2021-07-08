51f6f1fb08d04d85b8b51e3637e1758772c470d2
JgtL13 <65431751+JgtL13@users.noreply.github.com>
2021-07-08 15:17:03 +0800

    Update select-serv-v2.c

--- assignments/20190507/select-serv-v2.c
+++ assignments/20190507/select-serv-v2.c
@@ -4,125 +4,135 @@
 #define MAXCLI 6 
 int main(int argc , char *argv[])
 {
-WSADATA wsadata;
-SOCKET serv_sd , new_socket,cli_sd[MAXCLI];
-struct sockaddr_in serv, cli;
+	WSADATA wsadata;
+	SOCKET serv_sd , new_socket,cli_sd[MAXCLI];
+	struct sockaddr_in serv, cli;
 
-int activity, cli_len, i, j, n, client_num=0;
-fd_set readfds;
-char str[MAXRECV];
-char reject[MAXRECV] = "This room is currently full, please try again later.\n";
-char newMember[MAXRECV] = "A new member has entered the chat.\n";
+	int activity, cli_len, i, j, n, client_num=0;
+	fd_set readfds;
+	char str[MAXRECV];
+	char reject[MAXRECV] = "This room is currently full, please try again later.\n";
+	char newMember[MAXRECV] = "A new member has entered the chat.\n";
 
-for(i=0;i<MAXCLI;i++)
-	cli_sd[i]=0;
-
-WSAStartup(0x101, &wsadata);
-   	
-serv_sd=socket(AF_INET, SOCK_STREAM, 0);
-
-serv.sin_family = AF_INET;
-serv.sin_addr.s_addr = INADDR_ANY;
-serv.sin_port = htons( 5678 );
-
-bind(serv_sd, (LPSOCKADDR) &serv, sizeof(serv));
-
-listen(serv_sd , 3);
-
-
-while(TRUE)
-{
-	printf("[1]clear the socket fd set. \n");
-	FD_ZERO(&readfds);
-	
-	printf("[2]add serv_sd  to fd set \n");
-	FD_SET(serv_sd, &readfds);
-	
-	printf("[2]add cli_sd[]  to fd set \n");
 	for(i=0;i<MAXCLI;i++)
+		cli_sd[i]=0;
+
+	WSAStartup(0x101, &wsadata);
+
+	serv_sd=socket(AF_INET, SOCK_STREAM, 0);
+
+	serv.sin_family = AF_INET;
+	serv.sin_addr.s_addr = INADDR_ANY;
+	serv.sin_port = htons( 5678 );
+
+	bind(serv_sd, (LPSOCKADDR) &serv, sizeof(serv));
+
+	listen(serv_sd , 3);
+
+
+	while(TRUE)
 	{
-		if(cli_sd[i]>0)
+		printf("[1]clear the socket fd set. \n");
+		FD_ZERO(&readfds);
+
+		printf("[2]add serv_sd  to fd set \n");
+		FD_SET(serv_sd, &readfds);
+
+		printf("[2]add cli_sd[]  to fd set \n");
+		for(i=0;i<MAXCLI;i++)
 		{
-			FD_SET(cli_sd[i], &readfds);
+			if(cli_sd[i]>0)
+			{
+				FD_SET(cli_sd[i], &readfds);
+			}
+
 		}
-			
-	}
-		
-	printf("[3]call select() and waiting \n");
-	activity = select( 0 , &readfds , NULL , NULL , NULL);
-	printf("[4]wake up from select()\n");
-	
-	if ( activity == SOCKET_ERROR ){
-		printf("select call failed with error code : %d" , WSAGetLastError());
-		exit(EXIT_FAILURE);
-	}
-	
-	if (FD_ISSET(serv_sd , &readfds)){
-		cli_len = sizeof(cli);
-		new_socket = accept(serv_sd , (struct sockaddr *)&cli, (int *)&cli_len);
-		printf("New connection: socket fd is %d , ip is : %s , port : %d \n" , new_socket , 
+
+		printf("[3]call select() and waiting \n");
+		activity = select( 0 , &readfds , NULL , NULL , NULL);
+		printf("[4]wake up from select()\n");
+
+		if ( activity == SOCKET_ERROR )
+		{
+			printf("select call failed with error code : %d" , WSAGetLastError());
+			exit(EXIT_FAILURE);
+		}
+
+		if (FD_ISSET(serv_sd , &readfds))
+		{
+			cli_len = sizeof(cli);
+			new_socket = accept(serv_sd , (struct sockaddr *)&cli, (int *)&cli_len);
+			printf("New connection: socket fd is %d , ip is : %s , port : %d \n" , new_socket , 
 				inet_ntoa(cli.sin_addr) , ntohs(cli.sin_port));
-		for(i=0;i<MAXCLI;i++){
-			if(cli_sd[i]==0){
-				cli_sd[i]=new_socket;
-				client_num ++;
-				if(client_num == MAXCLI) //§PÂ_·í«e¥[¤Jªº¨Ï¥ÎªÌ¬O§_¶W¥X¤W­­
+			for(i=0;i<MAXCLI;i++)
+			{
+				if(cli_sd[i]==0)
 				{
-					send( cli_sd[i] , reject , strlen(reject)+1 , 0 ); //¶Ç°eÂ_½u³qª¾ 
-					closesocket(cli_sd[i]); //Â_½u 
-					cli_sd[i] = 0;  
-					client_num --;
+					cli_sd[i]=new_socket;
+					client_num ++;
+					if(client_num == MAXCLI) //åˆ¤æ–·ç•¶å‰åŠ å…¥çš„ä½¿ç”¨è€…æ˜¯å¦è¶…å‡ºä¸Šé™
+					{
+						send( cli_sd[i] , reject , strlen(reject)+1 , 0 ); //å‚³é€æ–·ç·šé€šçŸ¥ 
+						closesocket(cli_sd[i]); //æ–·ç·š 
+						cli_sd[i] = 0;  
+						client_num --;
+						break;
+					}
+					for(j = 0; j < client_num; ++j) //è‹¥æœ‰æ–°ä½¿ç”¨è€…åŠ å…¥ï¼Œé€šçŸ¥å…¶ä»–åœ¨ç·šä½¿ç”¨è€… 
+					{
+						if(cli_sd[j] != 0)
+						{
+							send( cli_sd[j] , newMember , strlen(newMember)+1 , 0 );
+						}
+					}
+					printf("The %d client socket is in cli_sd[%d]\n",client_num,i);
 					break;
 				}
-				for(j = 0; j < client_num; ++j) //­Y¦³·s¨Ï¥ÎªÌ¥[¤J¡A³qª¾¨ä¥L¦b½u¨Ï¥ÎªÌ 
-				{
-					if(cli_sd[j] != 0)
-					{
-						send( cli_sd[j] , newMember , strlen(newMember)+1 , 0 );
-					}
-				}
-				printf("The %d client socket is in cli_sd[%d]\n",client_num,i);
-				break;
 			}
 		}
-	}
-	
-	for(i=0;i<MAXCLI;i++){
-		if(FD_ISSET( cli_sd[i] , &readfds) ){
-			n = recv( cli_sd[i] , str, MAXRECV, 0);
-			if( n == SOCKET_ERROR){
-				int error_code = WSAGetLastError();
-				if(error_code == WSAECONNRESET){
-					printf("Host disconnected unexpectedly\n");
+
+		for(i=0;i<MAXCLI;i++)
+		{
+			if(FD_ISSET( cli_sd[i] , &readfds) )
+			{
+				n = recv( cli_sd[i] , str, MAXRECV, 0);
+				if( n == SOCKET_ERROR)
+				{
+					int error_code = WSAGetLastError();
+					if(error_code == WSAECONNRESET)
+					{
+						printf("Host disconnected unexpectedly\n");
+						closesocket( cli_sd[i] );	
+						cli_sd[i] = 0;
+						client_num --;
+					}
+					else
+						printf("recv failed with error code : %d" , error_code);				
+				}
+				if ( n == 0)
+				{
+					printf("Host disconnected. \n" );
 					closesocket( cli_sd[i] );	
 					cli_sd[i] = 0;
 					client_num --;
 				}
-				else
-					printf("recv failed with error code : %d" , error_code);				
-			}
-			if ( n == 0){
-				printf("Host disconnected. \n" );
-					closesocket( cli_sd[i] );	
-					cli_sd[i] = 0;
-					client_num --;
-			}
-			if (n > 0) {
-				for(j = 0; j < MAXCLI; ++j) //­Y¦³¨Ï¥ÎªÌµo¨¥¡A¨ä¥L¨Ï¥ÎªÌ¤]·|¦¬¨ì°T®§ 
+				if (n > 0) 
 				{
-					if(cli_sd[j] != 0)
+					for(j = 0; j < MAXCLI; ++j) //è‹¥æœ‰ä½¿ç”¨è€…ç™¼è¨€ï¼Œå…¶ä»–ä½¿ç”¨è€…ä¹Ÿæœƒæ”¶åˆ°è¨Šæ¯ 
 					{
-						send( cli_sd[j] , str , strlen(str)+1 , 0 );
+						if(cli_sd[j] != 0)
+						{
+							send( cli_sd[j] , str , strlen(str)+1 , 0 );
+						}
 					}
 				}
 			}
 		}
 	}
-}
-closesocket(serv_sd);
+	closesocket(serv_sd);
 
-WSACleanup();
-system("pause");
+	WSACleanup();
+	system("pause");
 
-return 0;
+	return 0;
 }
